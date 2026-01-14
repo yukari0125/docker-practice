cc8f0de276ea6f4bb77a2f0ba5b53aab8487dff6
yukari morinaga <j25.yu.sa@gmail.com>
2026-01-15 01:31:31 +0900

    確認テスト

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,17 +5,18 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
-Route::get('/', [ContactController::class, 'index'])->name('contact');
-
-Route::get('/contact', function () {
+Route::get('/', function () {
     return redirect()->route('contact');
 });
 
 Route::post('/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
+
 Route::post('/store', [ContactController::class, 'store'])->name('contact.store');
 
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('thanks');
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
@@ -23,11 +24,10 @@ Route::get('/register', [AuthController::class, 'showRegister'])->name('register
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 
 Route::middleware('auth')->group(function () {
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-Route::get('/search', [AdminController::class, 'search'])->name('admin.search');
-Route::get('/reset', [AdminController::class, 'reset'])->name('admin.reset');
-Route::delete('/delete', [AdminController::class, 'destroy'])->name('admin.destroy');
-Route::get('/export', [AdminController::class, 'export'])->name('admin.export');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/search', [AdminController::class, 'search'])->name('admin.search');
+    Route::get('/reset', [AdminController::class, 'reset'])->name('admin.reset');
+    Route::delete('/delete', [AdminController::class, 'destroy'])->name('admin.destroy');
+    Route::get('/export', [AdminController::class, 'export'])->name('admin.export');
 });
